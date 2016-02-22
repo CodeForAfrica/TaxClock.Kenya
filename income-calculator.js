83b47a7d53b06b9451832e874ea9ff3663e182d9
Greg Kempe <greg@kempe.net>
2016-02-22 16:08:23 +0200

    Stubs for income

--- income-calculator.js
+++ income-calculator.js
@@ -1,8 +1,10 @@
 var IncomeCalculator = function() {
   var self = this;
 
+  this.VAT = 0.14;
+
   this.calculateIncomeBreakdown = function(income) {
-    return {
+    var info = {
       income: income,
       breakdown: [{
           name: "Pay off SA's debts",
@@ -10,5 +12,30 @@ var IncomeCalculator = function() {
         },
       ]
     };
+
+    // income tax
+    info.incomeTax = self.incomeTax(info);
+    // after tax income
+    info.netIncome = income - info.incomeTax;
+
+    // VAT calculated on net income
+    info.vatTax = self.vatTax(info);
+
+    // total personal tax
+    info.personalTax = info.incomeTax + info.vatTax;
+    // income after tax and VAT
+    info.disposableIncome = income - info.personalTax;
+
+    return info;
+  };
+
+  this.incomeTax = function(info) {
+    // TODO
+    // return info.income * 0.3;  // 30%
+    return 69586;
+  };
+
+  this.vatTax = function(info) {
+    return info.netIncome * this.VAT;
   };
 };
