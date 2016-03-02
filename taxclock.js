f3aa69e4937f7977d07de7fb0fa8f50a36dda160
JD Bothma <jbothma@gmail.com>
2016-03-02 12:54:09 +0200

    Add opt-in embed link

---
+++ taxclock.js
@@ -0,0 +1,8 @@
+if (document.location.hostname == "localhost") {
+    var baseurl = "";
+} else {
+    var baseurl = "https://static.code4sa.org/taxclock/";
+}
+
+document.write('<script type="text/javascript" src="' + baseurl + 'pym.js"></script>')
+document.write("<script>var pymParent = new pym.Parent('code4sa-embed-taxclock', '" + baseurl + "index.html?show-link=true', {});</script>")
