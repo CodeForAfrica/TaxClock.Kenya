983aa39581cf3c14df63c614f35e360596ca974e
JD Bothma <jbothma@gmail.com>
2016-03-02 14:48:06 +0200

    embed example

--- embed.js
+++ embed.js
@@ -3,6 +3,5 @@ if (document.location.hostname == "localhost") {
 } else {
     var baseurl = "https://static.code4sa.org/taxclock/";
 }
-
-document.write('<script type="text/javascript" src="' + baseurl + 'pym.js"></script>')
-document.write("<script>var pymParent = new pym.Parent('code4sa-embed-taxclock', '" + baseurl + "index.html', {});</script>")
+document.write('<script type="text/javascript" src="' + baseurl + 'pym.js"></script>');
+document.write("<script>var pymParent = new pym.Parent('code4sa-embed-taxclock', '" + baseurl + "index.html', {});</script>");
--- taxclock.js
+++ taxclock.js
@@ -3,6 +3,6 @@ if (document.location.hostname == "localhost") {
 } else {
     var baseurl = "https://static.code4sa.org/taxclock/";
 }
-
-document.write('<script type="text/javascript" src="' + baseurl + 'pym.js"></script>')
-document.write("<script>var pymParent = new pym.Parent('code4sa-embed-taxclock', '" + baseurl + "index.html?show-link=true', {});</script>")
+document.write('<div id="code4sa-embed-taxclock"></div>');
+document.write('<script type="text/javascript" src="' + baseurl + 'pym.js"></script>');
+document.write("<script>var pymParent = new pym.Parent('code4sa-embed-taxclock', '" + baseurl + "index.html?show-embed-link=true', {});</script>");
