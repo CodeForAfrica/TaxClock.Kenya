751959ffa2c693584e7bbd838a5eea483950b284
David Lemayian <dlemayian@gmail.com>
2019-04-17 10:34:14 +0300

    Load PesaCheck stories.

--- js/taxclock/tc-stories.js
+++ js/taxclock/tc-stories.js
@@ -8,14 +8,29 @@ TC.stories = {
   data: [],
 
   load: function () {
-    var base_url = '';
-    if (document.location.hostname == 'localhost') {
-      base_url = 'http://{{ site.enforce_ssl }}';
-    };
     $.get(
-      base_url + '/data/standard-news.json',
-      function( data ) {
-        TC.stories.data = JSON.parse(data);
+      "https://cors.io?https://pesacheck.org/feed/tagged/budget",
+      function( response ) {
+        parser = new DOMParser();
+        xmlDoc = parser.parseFromString(response,"text/xml");
+        stories = xmlDoc.getElementsByTagName("item")
+
+        stories_count = stories.length > 5 ? 5 : stories.length
+
+        for (let i = 0; i < stories_count; i++) {
+          title = stories[i].getElementsByTagName("title")[0].childNodes[0].nodeValue;
+          link = stories[i].getElementsByTagName("link")[0].childNodes[0].nodeValue;
+          content = stories[i].getElementsByTagName("content:encoded")[0].childNodes[0].nodeValue;
+          content_html = parser.parseFromString(content,"text/html");
+          thumbnail = content_html.getElementsByTagName("img")[0].getAttribute("src");
+
+          TC.stories.data.push({
+            "title": title,
+            "link": link,
+            "thumbnail": thumbnail
+          })
+        }
+
         TC.stories.show();
       }
     );
@@ -29,9 +44,9 @@ TC.stories = {
       html += '<div class="tc-box text-left tc-story tc-box-white">';
       html += '<a href="' + data.link + '" target="_blank">';
       html += '<div class="row"><div class="col-xs-4">';
-      html += '<img src="' + data.img + '"/>';
+      html += '<img src="' + data.thumbnail + '"/>';
       html += '</div><div class="col-xs-8">';
-      html += '<h4>' + data.title + '</h4><p>';// + data.description + '</p>';
+      html += '<h4>' + data.title + '</h4>';
       html += '<p><u>Read more</u> <i class="fa fa-arrow-right"></i></p>';
       html += '</div></div></a></div><br/>'
     });
